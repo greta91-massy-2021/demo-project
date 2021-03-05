6541da5ade8b9796307abda6d01fbc3aef7edbb6
Soupramanien BOUVANESVARY <soupramanien@baobab-ingenierie.fr>
2021-03-05 11:40:19 +0100

    life cycle methods added

--- app/src/main/java/fr/baobab/greta91demo/MainActivity.java
+++ app/src/main/java/fr/baobab/greta91demo/MainActivity.java
@@ -1,11 +1,13 @@
 package fr.baobab.greta91demo;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +16,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public static final int SECOND_ACTIVITY = 1;
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.complex_layout);
+        Log.i(TAG, "dans onCreate");
         final Button button = (Button)findViewById(R.id.button);//récupère le bouton 1
         final Button button2 = (Button)findViewById(R.id.button2);//récupère le bouton 2
         final Button button3 = (Button)findViewById(R.id.button3);//récupère le bouton 3
@@ -46,6 +50,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button4.setOnClickListener(this);
         button5.setOnClickListener(this);
         button6.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "dans onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "dans onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "dans onResume");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "dans onSaveInstanceState");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "dans onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "dans onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "dans onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "dans onDestroy");
     }
 
     @Override
